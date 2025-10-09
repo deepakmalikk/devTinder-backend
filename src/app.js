b712d98ae49e74b521824a33be25fd4883b62508
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-10-09 20:08:45 +0530

    request handler

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 4001
+const express = require("express");
 
- 
-app.get("/user",(req, res) => {
-    res.send([{
-        "name":"deepak",
-        "lastName": "malik",
-        "age":"25"
-    }])
-  })
+const app =express();
+const port = 3000;
 
-  app.post("/user",(req,res)=>{
-    res.send("data saved Sucessfully")
-  })
+app.use("/user",(req,res,next)=>{
+    console.log("route handler 1");
+    next();
+},
+(req,res,next)=>{
+    console.log("route handler 2");
+    next();
+},
+(req,res,next)=>{
+    res.send("response from 3")
+}
 
-  app.delete("/user",(req,res)=>{
-    res.send("deleted sucessfully.")
-  })
+)
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port,()=>{
+    console.log(`listing port ${port}`)
+});
