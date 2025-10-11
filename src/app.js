b4890234a853db0e134280e6799e29253c315c6c
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-10-11 15:45:11 +0530

    Feature: to fetch users

--- src/app.js
+++ src/app.js
@@ -4,12 +4,27 @@ const app = express();
 const connectDB = require("./config/database")
 
 app.use(express.json());
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    // To find specific user
+    // const user = await User.find({ email: userEmail });
+
+    // To find all user
+    const users = await User.find({});
+    res.send(users);
+    if (users.length === 0) {
+      res.status(404).send("Users not found");
+    }
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+// TO put dynamic data into database
 app.post("/signup",async (req,res)=>{
     const user = new User(req.body);
-    
-    
     try{await user.save();
-
     res.status(200).json({
         message:"User created successfully"
     })
