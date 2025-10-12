9c371ffa59b64fcc025b1a71190df604fb302b57
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-10-12 21:31:17 +0530

    added necessary packages

---
+++ src/middlewares/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const userAuth= async (req,res,next)=>{
+    
+    
+    try{
+    // Read the token from the req cookies
+
+    const {token} = req.cookies;
+    if(!token){
+        throw new Error("Invalid Token");
+    }
+    // Validate the token
+    const decodedObj = await jwt.verify(token,"deepak@12");
+    
+    const _id = decodedObj;
+    // Find User
+    const user = await User.findById(_id);
+
+    if(!user){
+        throw new Error("User not found")
+    }
+
+    req.user = user;
+    next();
+}catch(err){
+    res.status(401).send("Error:" + err.message);
+}
+        
+    
+}
+module.exports ={
+    userAuth
+}
