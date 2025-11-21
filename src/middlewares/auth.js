0aa48690cb54ce3e32767f217307df1684ec606e
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-11-21 22:22:08 +0530

    change code with dotenv variable

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,7 +9,7 @@ const userAuth = async (req, res, next) => {
     }
 
     const decodedObj = await jwt.verify(token, process.env.JWT_SECRET)
-
+    console.log(process.env.JWT_SECRET)
     const { _id } = decodedObj;
 
     const user = await User.findById(_id);
