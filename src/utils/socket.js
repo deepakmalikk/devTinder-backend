9219d15c631f8bf1301f36cd0c79b5721afa1597
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-11-28 20:11:32 +0530

    hashed the room id

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,10 @@
 const socket =require("socket.io");
+const cryto = require("crypto");
 
+const getSecretRoomId = (userId, targetUserId) => {
+
+  return cryto.createHash('sha256').update([userId,targetUserId].sort().join("_")).digest('hex');
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -12,14 +17,14 @@ const initializeSocket = (server) => {
   
     
     socket.on("joinChat",([userId, targetUserId])=>{
-        const roomId = [userId,targetUserId].sort().join("_");
+        const roomId = getSecretRoomId(userId, targetUserId);
 
         socket.join(roomId);
         console.log(`User ${userId} joined room ${roomId}`);
     });
 
     socket.io("sendMessage", (firstName, userId, targetUserId, message)=>{
-        const roomId = [userId, targetUserId].sort().join("_");
+        const roomId = getSecretRoomId(userId,targetUserId)
         io.to(roomId).emit(" ", { firstName, message });
         console.log(`Message from ${firstName}: ${message}`);
     });
