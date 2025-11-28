440998c3988b70389cf856d6630a6d0f9bf0df7b
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-11-28 22:16:35 +0530

    msg save in db

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -29,10 +29,31 @@ const initializeSocket = (server) => {
       console.log(`User ${userId} joined room ${roomId}`);
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, message }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      io.to(roomId).emit("newMessage", { firstName, message, userId, targetUserId });
-      console.log(`Message from ${firstName}: ${message}`);
+    socket.on("sendMessage", async({ firstName, userId, targetUserId, message }) => {
+      try {
+         const roomId = getSecretRoomId(userId, targetUserId);
+         let chat = await Chat.findOne({
+        participants: { $all: [userId, targetUserId]},
+      })
+
+      if(!chat){
+        chat = new Chat({
+          participants: [userId, targetUserId],
+          messages: [],
+        })
+      }
+      chat.message.push({
+        senderId:  userId,
+        message,
+      })
+        await chat.save();
+         io.to(roomId).emit("newMessage", { firstName, message, userId, targetUserId });
+         console.log(`Message from ${firstName}: ${message}`);
+
+      } catch (error) {
+        console.error("Error saving message:", error);
+      }
+     
     });
 
     socket.on("disconnect", () => {
