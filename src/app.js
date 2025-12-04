a4413f6b8a83b32c46488cae7562f7fb8c85450b
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-12-04 15:50:06 +0530

    removed extra path

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use("/api", requestRouter);
 app.use("/api", userRouter);
 app.use("/api", chatRouter);
 
- const path = require("path");
+
 
 // Serve static frontend
 app.use(express.static(path.join(__dirname, "dist"))); // or "build" depending on your build
