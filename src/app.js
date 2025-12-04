a4d90b6c7d9accc8ffcef71d3447d689b443b0cb
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-12-04 18:31:42 +0530

    removed fallback code

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const chatRouter = require("./routes/chat");
 const app = express();
 const http = require("http");
 const initializeSocket = require("./utils/socket");
-const path = require("path");
+ 
 const server = http.createServer(app);
 
  
@@ -46,17 +46,8 @@ app.use("/api", userRouter);
 app.use("/api", chatRouter);
 
 
-
-// Serve static frontend
-app.use(express.static(path.join(__dirname, "dist"))); // or "build" depending on your build
-
-// Catch-all: send index.html for any non-API route
-app.get("*", (req, res) => {
-  if (req.path.startsWith("/api")) {
-    return res.status(404).send("API route not found");
-  }
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+ 
+ 
 
 
 app.get("/health", (req, res) => {
