d469d3b059c8f8ea5210c06ab6d3e5bcac12362b
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-12-05 23:16:54 +0530

    modified default about user

--- src/models/user.js
+++ src/models/user.js
@@ -69,7 +69,7 @@ const userSchema = new mongoose.Schema(
     },
     about: {
       type: String,
-      default: "This is a default about of the user!",
+      default: "Passionate about building, learning, and collaborating.",
     },
     skills: {
       type: [String],
